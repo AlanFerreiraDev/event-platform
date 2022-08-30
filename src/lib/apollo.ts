108cd2d04415872ba0e652ace13223dadc90a597
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2022-08-30 19:14:44 -0300

    fix: deletar arquivo e refatorando o local

--- src/lib/apollo.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-
-export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.hygraph.com/v2/cl7fc3ekx6ss801uh3kry40nv/master',
-  cache: new InMemoryCache(),
-})
